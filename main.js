9962ac8603d6bd57c92fbd37040fe9545f0b5fb4
naplazz <naplazz@gmail.com>
2019-06-05 22:24:05 +0200

    mod

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+
+$(document).ready(function refresh() {
+  function displayCharts(){
   var url_base = 'http://157.230.17.132:4016/sales';
   var fatturato_mensile = {
     '1': 0,
@@ -14,7 +16,12 @@ $(document).ready(function() {
     '11': 0,
     '12': 0,
   }
-
+  var fatturato_venditori ={
+    Riccardo: 0,
+    Roberto: 0,
+    Marco: 0,
+    Giuseppe: 0
+  };
   var oggetto_data = {
     labels: [],
     datasets: [{
@@ -22,12 +29,27 @@ $(document).ready(function() {
       data: [],
     }]
   };
-  var fatturato_venditori = [];
+
   var data_venditori = {
     labels: [],
     datasets: [{
       label: 'Vendite Per venditore',
       data: [],
+      backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+
+            ],
+            borderWidth: 1
     }]
   };
 
@@ -39,25 +61,42 @@ $(document).ready(function() {
       for (var i = 0; i < data.length; i++) {
         var mese = moment(data[i].date, 'DD/MM/YYYY').format('M');
         var venditore = data[i].salesman;
-        var valoreVendita = data[i].amount;
+        var valoreVendita = parseInt(data[i].amount);
         for (x in fatturato_mensile) {
           if (x == mese) {
             fatturato_mensile[x] += valoreVendita
           } //chiuso if
         } //chiuso forin
-        if (!fatturato_venditori.includes(venditore)) {
-            fatturato_venditori.push([venditore,valoreVendita])
-        }
+        for (z in fatturato_venditori) {
+          if (z == venditore) {
+            fatturato_venditori[z] += valoreVendita
+          } //chiuso if
+        } //chiuso forin
+
+        var source   = document.getElementById("entry-template").innerHTML;
+        var template = Handlebars.compile(source);
+        var context = {
+          venditore: venditore,
+          data: data[i].date,
+          valore: valoreVendita
+        };
+        var html    = template(context);
+        $('tbody').append(html)
       } //chiuso for
-      console.log(fatturato_venditori)
+
       //push dei dati
       for (var y in fatturato_mensile) {
         oggetto_data.datasets[0].data.push(fatturato_mensile[y]);
         var nomimesi = moment(y, 'M').format('MMMM');
         oggetto_data.labels.push(nomimesi);
       }
+      for (var z in fatturato_venditori) {
+        data_venditori.datasets[0].data.push(fatturato_venditori[z]);
+        data_venditori.labels.push(z);
+      }
       //=============chart 1==============
-      var ctx = $('#myChart')[0].getContext('2d');
+
+      var ctx = document.getElementById('myChart').getContext('2d');
       var chart = new Chart(ctx, {
         // The type of chart we want to create
         type: 'line',
@@ -82,4 +121,56 @@ $(document).ready(function() {
       alert('errore');
     } // closing error
   }); // closing ajax
+
+}; // closing displayCharts
+displayCharts()
+//========================================= inserimento vendita ============
+
+  $('#btn').click(function() {
+    var newsaleSeller = $('.select-seller').val();
+    var newsaleData = $('#datevendita').val()
+
+    var newsaleAmount = $('#amount').val()
+      console.log(newsaleAmount)
+      $.ajax({
+        url: 'http://157.230.17.132:4016/sales',
+        method: 'POST',
+        data: {
+          'salesman': newsaleSeller,
+          'amount': newsaleAmount,
+          'date': newsaleData
+        },
+        success: function () {
+          alert('inserito')
+        },
+        error: function(){
+          alert('errore')
+        }
+
+      }); // closing ajax post
+
+
+
+}); //closing click
+
+//========================================= delete sale ============
+
+$('#btnDel').click(function() {
+  var saleId = parseInt($('#delete').val())
+
+    $.ajax({
+      url: 'http://157.230.17.132:4016/sales/'+ saleId,
+      method: 'DELETE',
+      success: function () {
+        alert('eliminato')
+      },
+      error: function(){
+        alert('errore')
+      }
+
+    }); // closing ajax post
+
+}); //closing click
+
+
 }); //doc ready close
